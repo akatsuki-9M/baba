87a1f68602f5c43582361722fc20a226e6059f62
Nikhiltewary <122506536+Nikhiltewary@users.noreply.github.com>
2023-10-08 18:16:39 +0530

    Update fucky.py

--- fucky.py
+++ fucky.py
@@ -1,8 +1,10 @@
 import random
 import string
- domains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'outlook.com', 'aol.com']
+ domains = ['gmail.com', 'xnxx.xom', 'hotmail.com', 'outlook.com', 'aol.com']
  def generate_random_email():
     letters = string.ascii_lowercase
+  def term1():
+   domain = random.choice(domains)
     username = ''.join(random.choice(letters) for i in range(8))
     domain = random.choice(domains)
     return username + '@' + domain
